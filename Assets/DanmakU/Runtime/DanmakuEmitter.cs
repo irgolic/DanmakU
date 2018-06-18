727b15fb0442e891297c18f945c445762d173193
Rafael Irgolic <rafael.irgolic@gmail.com>
2018-06-18 21:50:18 +0200

    protected

--- Assets/DanmakU/Runtime/DanmakuEmitter.cs
+++ Assets/DanmakU/Runtime/DanmakuEmitter.cs
@@ -32,16 +32,16 @@ namespace DanmakU
                 set.Pool.TeamNo = TeamNo = newTeam;
         }
 
-        float timer;
+        protected float timer;
 
-        DanmakuConfig config;
-        IFireable fireable;
+        protected DanmakuConfig config;
+        protected IFireable fireable;
 
         /// <summary>
         /// Start is called on the frame when a script is enabled just before
         /// any of the Update methods is called the first time.
         /// </summary>
-        void Start()
+        protected void Start()
         {
             if (DanmakuType == null)
             {
