618a2052f989087b1e4fe61b3b7bc478d50265f0
Rafael Irgolic <rafael.irgolic@gmail.com>
2018-06-18 21:34:01 +0200

    reformat

--- Assets/DanmakU/Runtime/Colliders/TeamCollider.cs
+++ Assets/DanmakU/Runtime/Colliders/TeamCollider.cs
@@ -4,81 +4,77 @@ using UnityEngine;
 
 namespace DanmakU
 {
-
-  public class TeamCollider : MonoBehaviour
-  {
-
-    public DanmakuCollider Collider;
-    public DanmakuEmitter Emitter;
-
-    //todo: make a thorough pass focusing on accessibility
-    //todo: thorough update to danmaku
-    
-    /// <summary>
-    /// This function is called when the object becomes enabled and active.
-    /// </summary>
-    void OnEnable()
+    public class TeamCollider : MonoBehaviour
     {
-      if (Collider != null)
-      {
-        Debug.Log("Subscribed");
-        Collider.OnDanmakuCollision += OnDanmakuCollision;
-      }
-    }
+        public DanmakuCollider Collider;
+        public DanmakuEmitter Emitter;
 
-    /// <summary>
-    /// This function is called when the behaviour becomes disabled or inactive.
-    /// </summary>
-    void OnDisable()
-    {
-      if (Collider != null)
-      {
-        Collider.OnDanmakuCollision -= OnDanmakuCollision;
-      }
-    }
+        //todo: make a thorough pass focusing on accessibility
+        //todo: thorough update to danmaku
 
-
-    void getHit()
-    {
-      gameObject.SetActive(false);
-    }
-
-    // Update is called once per frame
-    // void Update()
-    // {
-    /*if (invulnTimer > 0f)
-    {
-      invulnTimer -= Time.deltaTime;
-      if (invulnTimer < 0f)
-      {
-        invulnTimer = 0f;
-        //sprite.color = Color.white;
-      }
-    }*/
-    // }
-
-    void OnDanmakuCollision(DanmakuCollisionList collisions)
-    {
-      //if (emitter)
-      foreach (var collision in collisions)
-      {
-        if (collision.Danmaku.Pool.TeamNo != Emitter.set.Pool.TeamNo)
+        /// <summary>
+        /// This function is called when the object becomes enabled and active.
+        /// </summary>
+        void OnEnable()
         {
-          collision.Danmaku.Destroy();
-          getHit();
+            if (Collider != null)
+            {
+                Debug.Log("Subscribed");
+                Collider.OnDanmakuCollision += OnDanmakuCollision;
+            }
         }
-      }
+
+        /// <summary>
+        /// This function is called when the behaviour becomes disabled or inactive.
+        /// </summary>
+        void OnDisable()
+        {
+            if (Collider != null)
+            {
+                Collider.OnDanmakuCollision -= OnDanmakuCollision;
+            }
+        }
+
+
+        void getHit()
+        {
+            gameObject.SetActive(false);
+        }
+
+        // Update is called once per frame
+        // void Update()
+        // {
+        /*if (invulnTimer > 0f)
+        {
+          invulnTimer -= Time.deltaTime;
+          if (invulnTimer < 0f)
+          {
+            invulnTimer = 0f;
+            //sprite.color = Color.white;
+          }
+        }*/
+        // }
+
+        void OnDanmakuCollision(DanmakuCollisionList collisions)
+        {
+            //if (emitter)
+            foreach (var collision in collisions)
+            {
+                if (collision.Danmaku.Pool.TeamNo != Emitter.set.Pool.TeamNo)
+                {
+                    collision.Danmaku.Destroy();
+                    getHit();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Reset is called when the user hits the Reset button in the Inspector's
+        /// context menu or when adding the component the first time.
+        /// </summary>
+        void Reset()
+        {
+            Collider = GetComponent<DanmakuCollider>();
+        }
     }
-
-    /// <summary>
-    /// Reset is called when the user hits the Reset button in the Inspector's
-    /// context menu or when adding the component the first time.
-    /// </summary>
-    void Reset()
-    {
-      Collider = GetComponent<DanmakuCollider>();
-    }
-
-  }
-
 }
