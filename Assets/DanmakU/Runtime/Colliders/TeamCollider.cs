d3cd8c3df9723306226a1dfc5fe8d97c94776fad
Rafael Irgolic <rafael.irgolic@gmail.com>
2018-06-19 19:37:37 +0200

    cleanup

--- Assets/DanmakU/Runtime/Colliders/TeamCollider.cs
+++ Assets/DanmakU/Runtime/Colliders/TeamCollider.cs
@@ -2,61 +2,61 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace DanmakU
+namespace DanmakU {
+public class TeamCollider : MonoBehaviour
 {
-    public class TeamCollider : MonoBehaviour
+  public DanmakuCollider Collider;
+  public int TeamNo;
+
+  /// <summary>
+  /// This function is called when the object becomes enabled and active.
+  /// </summary>
+  void OnEnable()
+  {
+    if (Collider != null)
     {
-        public DanmakuCollider Collider;
-        public int TeamNo;
-
-        /// <summary>
-        /// This function is called when the object becomes enabled and active.
-        /// </summary>
-        void OnEnable()
-        {
-            if (Collider != null)
-            {
-                Debug.Log("Subscribed");
-                Collider.OnDanmakuCollision += OnDanmakuCollision;
-            }
-        }
-
-        /// <summary>
-        /// This function is called when the behaviour becomes disabled or inactive.
-        /// </summary>
-        void OnDisable()
-        {
-            if (Collider != null)
-            {
-                Collider.OnDanmakuCollision -= OnDanmakuCollision;
-            }
-        }
-
-        void getHit()
-        {
-            gameObject.SetActive(false);
-        }
-
-
-        void OnDanmakuCollision(DanmakuCollisionList collisions)
-        {
-            foreach (var collision in collisions)
-            {
-                if (collision.Danmaku.Pool.TeamNo != TeamNo)
-                {
-                    collision.Danmaku.Destroy();
-                    getHit();
-                }
-            }
-        }
-
-        /// <summary>
-        /// Reset is called when the user hits the Reset button in the Inspector's
-        /// context menu or when adding the component the first time.
-        /// </summary>
-        void Reset()
-        {
-            Collider = GetComponent<DanmakuCollider>();
-        }
+      Debug.Log("Subscribed");
+      Collider.OnDanmakuCollision += OnDanmakuCollision;
     }
+  }
+
+  /// <summary>
+  /// This function is called when the behaviour becomes disabled or inactive.
+  /// </summary>
+  void OnDisable()
+  {
+    if (Collider != null)
+    {
+      Collider.OnDanmakuCollision -= OnDanmakuCollision;
+    }
+  }
+
+  void getHit()
+  {
+    gameObject.SetActive(false);
+  }
+
+
+  void OnDanmakuCollision(DanmakuCollisionList collisions)
+  {
+    foreach (var collision in collisions)
+    {
+      if (collision.Danmaku.Pool.TeamNo != TeamNo)
+      {
+        collision.Danmaku.Destroy();
+        getHit();
+      }
+    }
+  }
+
+  /// <summary>
+  /// Reset is called when the user hits the Reset button in the Inspector's
+  /// context menu or when adding the component the first time.
+  /// </summary>
+  void Reset()
+  {
+    Collider = GetComponent<DanmakuCollider>();
+  }
+}
+  
 }
